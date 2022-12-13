ff8220f2b7d5b8d6bf86b01263913d902b78f435
Gustavo Pereira Marcena <111616576+GustavoPMarcena@users.noreply.github.com>
2022-12-13 11:23:47 -0300

    up

---
+++ biblioteca.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+
+class Livro:
+    def __init__(self, nome, genero, autor):
+         self.nome = nome
+         self.genero = genero
+         self.autor = autor
+
+livro1 = Livro('Você', 'Romance', 'Michele')
+livro2 = Livro('Classrom of the elite', 'Romance', 'C. S. Lewis')
+livro3 = Livro('Jojo bizarre adventures', 'Ação', 'Araki')
+lista = [livro1, livro2, livro3]
+
+app = Flask(__name__)
+
+app.secret_key= 'ifpb2022'
+
+
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo="livros", livros=lista)
+
+@app.route('/cadastro')
+def cadastro():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('cadastro')))
+    return render_template('cadastro.html', titulo='Novo Livro')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+   nome = request.form['nome']
+   genero = request.form['genero']
+   autor = request.form['autor']
+   livro = Livro(nome, genero, autor)
+   lista.append(livro)
+   return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', titulo='Login', proxima = proxima)
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if '12345' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso')
+        proxima_pagina = request.form['proxima']
+        return redirect(proxima_pagina)
+    else:
+        flash('Credenciais erradas')
+        return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+   session['usuario_logado'] = None
+   flash('Logout efetuado com sucesso!')
+   return redirect(url_for('login'))
+
+app.run(debug=True)
